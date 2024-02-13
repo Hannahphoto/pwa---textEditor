da366ab4ff938145820c1f56a58bf0d1ddde592e
Hannahphoto <hannahprice67@gmail.com>
2024-02-13 17:03:17 -0500

    completed webpack confic file and sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,19 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+//Implement asset caching
+registerRoute(
+  //Define callback function that will filter the requests we want to cache (JS & CSS files
+  ({request})=>['style', 'script', 'worker'].includes(request.destination),
+  new offlineFallback({
+    //Name of cache storage
+    cacheName: 'asset-cache',
+    plugins: [
+      //This plug-m will cache response with these headers to maximum age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0,200],
+      }),
+    ],
+  })
+  );
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
@@ -17,13 +17,59 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
+    //Add and configure workbox plugins for a service worker and manifest file.
+    plugins: 
+            [
+      //Webpack plugin that generates our htm file and insers our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      //Injects our custome service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        title: 'Text Editor',
+        short_name: 'Editor',
+        description: 'A quick way to check and edit your text.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
+    //Add Babel to webpack.
     module: {
+      //Add CSS loaders
       rules: [
-        
+        {
+          tests: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        //Use babel-loader in order to use ES6
+        {
+          use:{
+            loader: 'babel-laoder',
+            options:{
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
